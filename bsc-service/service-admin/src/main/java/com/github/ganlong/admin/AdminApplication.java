b50edb6c68038ced7d2425cffb726d086ed64de9
根号三 <3237764237@qq.com>
2022-11-19 18:12:37 +0800

    更新授权模块

---
+++ bsc-service/service-admin/src/main/java/com/github/ganlong/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package com.github.ganlong.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author KnockingFarmers
+ * @Date 2022/11/19 18:06
+ * @PackageName:com.github.ganlong.admin
+ * @ClassName: AdminApplication
+ * @Description: TODO
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class,args);
+    }
+}
