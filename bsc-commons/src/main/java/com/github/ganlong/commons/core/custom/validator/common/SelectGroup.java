5f45d114cb22ddbebd53234b0c001caa88744216
根号三 <3237764237@qq.com>
2022-12-19 09:30:31 +0800

    新增JSR303参数校验

---
+++ bsc-commons/src/main/java/com/github/ganlong/commons/core/custom/validator/common/SelectGroup.java
@@ -0,0 +1,14 @@
+package com.github.ganlong.commons.core.custom.validator.common;
+
+import javax.validation.groups.Default;
+
+/**
+ * @Author KnockingFarmers
+ * @Date 2022/12/19 9:28
+ * @PackageName:com.github.ganlong.commons.core.custom.validator.common
+ * @ClassName: SelectGroup
+ * @Description: TODO
+ * @Version 1.0
+ */
+public interface SelectGroup extends Default {
+}
